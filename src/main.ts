995a16d515cb7c21580e1796a5f78524a2475665
seitamuro <seitamuro0210@gmail.com>
2023-11-07 20:07:43 +0900

    物理演算を追加

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { Engine } from "@babylonjs/core/Engines/engine";
 import { Scene } from "@babylonjs/core/scene";
-import { Vector3 } from "@babylonjs/core/Maths/math";
+import { Color3, Vector3 } from "@babylonjs/core/Maths/math";
 import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
 import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
-import { MeshBuilder } from "@babylonjs/core";
+import { CannonJSPlugin, MeshBuilder, StandardMaterial } from "@babylonjs/core";
+import { PhysicsImpostor } from "@babylonjs/core";
+
+import * as CANNON from "cannon";
 
 import "./style.css";
 
@@ -15,6 +18,10 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const engine = new Engine(canvas, true);
 const scene = new Scene(engine);
+scene.enablePhysics(
+  new Vector3(0, -9.81, 0),
+  new CannonJSPlugin(true, 10, CANNON)
+);
 
 const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
 camera.setTarget(Vector3.Zero());
@@ -23,7 +30,30 @@ camera.attachControl(canvas, true);
 const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
 light.intensity = 0.7;
 
+const ground = MeshBuilder.CreateBox(
+  "ground",
+  { width: 10, height: 1, depth: 10 },
+  scene
+);
+ground.position.y = -2;
+const groundMaterial = new StandardMaterial("groundMaterial", scene);
+groundMaterial.diffuseColor = new Color3(1, 0, 0);
+ground.material = groundMaterial;
+ground.physicsImpostor = new PhysicsImpostor(
+  ground,
+  PhysicsImpostor.BoxImpostor,
+  { mass: 0, restitution: 0.2 },
+  scene
+);
+
 const box = MeshBuilder.CreateBox("box", { size: 2 }, scene);
+box.position.y = 1;
+box.physicsImpostor = new PhysicsImpostor(
+  box,
+  PhysicsImpostor.BoxImpostor,
+  { mass: 1, restitution: 0.2 },
+  scene
+);
 
 engine.runRenderLoop(() => {
   scene.render();
