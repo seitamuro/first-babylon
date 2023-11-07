28e7d2c0f6081701fc5c4c89a3463ab841446951
seitamuro <seitamuro0210@gmail.com>
2023-11-07 19:55:08 +0900

    四角形を表示

--- src/main.ts
+++ src/main.ts
@@ -1,81 +1,34 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import vertexShader from "./shader/vertexShader.vert";
-import fragmentShader from "./shader/fragmentShader.frag";
+import { Engine } from "@babylonjs/core/Engines/engine";
+import { Scene } from "@babylonjs/core/scene";
+import { Vector3 } from "@babylonjs/core/Maths/math";
+import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { MeshBuilder } from "@babylonjs/core";
+
 import "./style.css";
 
 /**
  * Setup Scene , Camera and etc
  */
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
 const canvas = document.querySelector(".webgl") as HTMLCanvasElement;
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-camera.position.set(0.25, -0.25, 1);
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const engine = new Engine(canvas, true);
+const scene = new Scene(engine);
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
+const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+camera.setTarget(Vector3.Zero());
+camera.attachControl(canvas, true);
+
+const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+light.intensity = 0.7;
+
+const box = MeshBuilder.CreateBox("box", { size: 2 }, scene);
+
+engine.runRenderLoop(() => {
+  scene.render();
 });
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
 window.addEventListener("resize", () => {
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  engine.resize();
 });
-scene.add(camera);
-
-/**
- * Geometry
- */
-const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
-const count = geometry.attributes.position.count;
-
-const aRandom = new Float32Array(count);
-for (let i = 0; i < count; i++) {
-  aRandom[i] = Math.random();
-}
-geometry.setAttribute("aRandom", new THREE.BufferAttribute(aRandom, 1));
-
-const material = new THREE.RawShaderMaterial({
-  vertexShader: vertexShader,
-  fragmentShader: fragmentShader,
-  transparent: true,
-  side: THREE.DoubleSide,
-  uniforms: { uTime: { value: 0.0 } },
-});
-
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-/**
- * Animation
- */
-const clock = new THREE.Clock();
-
-const animate = () => {
-  const elapsedTime = clock.getElapsedTime();
-
-  material.uniforms.uTime.value = elapsedTime;
-
-  controls.update();
-  renderer.render(scene, camera);
-  window.requestAnimationFrame(animate);
-};
-
-animate();
