c2e7a74f2ce99372cc50e9b2012270729571ea2d
seitamuro <seitamuro0210@gmail.com>
2023-11-09 09:41:40 +0900

    may object collision detection

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { Scene } from "@babylonjs/core/scene";
 import { Color3, Vector3 } from "@babylonjs/core/Maths/math";
 import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
 import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
-import { CannonJSPlugin, MeshBuilder, StandardMaterial } from "@babylonjs/core";
+import {
+  CannonJSPlugin,
+  MeshBuilder,
+  Mesh,
+  StandardMaterial,
+} from "@babylonjs/core";
 import { PhysicsImpostor } from "@babylonjs/core";
 
 import * as CANNON from "cannon";
@@ -35,7 +40,7 @@ light.intensity = 0.7;
  */
 const ground = MeshBuilder.CreateBox(
   "ground",
-  { width: 10, height: 1, depth: 10 },
+  { width: 30, height: 1, depth: 30 },
   scene
 );
 ground.position.y = -2;
@@ -49,24 +54,46 @@ ground.physicsImpostor = new PhysicsImpostor(
   scene
 );
 
-const box = MeshBuilder.CreateBox("box", { size: 2 }, scene);
-const boxMaterial = new StandardMaterial("boxMaterial", scene);
-box.material = boxMaterial;
-box.position.y = 1;
-box.physicsImpostor = new PhysicsImpostor(
-  box,
-  PhysicsImpostor.BoxImpostor,
-  { mass: 1, restitution: 0.2 },
-  scene
-);
+interface BoxMesh extends Mesh {
+  material: StandardMaterial;
+}
 
-scene.onBeforeRenderObservable.add(() => {
-  if (box.intersectsMesh(ground)) {
-    boxMaterial.diffuseColor = new Color3(1, 0, 0);
-  } else {
-    boxMaterial.diffuseColor = new Color3(0.5, 0.5, 0.5);
-  }
-});
+const boxGroup: BoxMesh[] = [];
+
+for (let i = 0; i < 10; i++) {
+  const box = MeshBuilder.CreateBox(`box`, { size: 2 }, scene) as BoxMesh;
+  const boxMaterial = new StandardMaterial("boxMaterial", scene);
+  box.material = boxMaterial;
+  box.position.x = Math.random() * 20 - 5;
+  box.position.y = 5 + Math.random() * 5;
+  box.position.z = Math.random() * 20 - 5;
+  box.rotate(new Vector3(1, 1, 1), Math.random() * Math.PI * 2);
+  box.physicsImpostor = new PhysicsImpostor(
+    box,
+    PhysicsImpostor.BoxImpostor,
+    { mass: 1, restitution: 0.9 },
+    scene
+  );
+  boxGroup.push(box);
+}
+
+for (let box of boxGroup) {
+  scene.onBeforeRenderObservable.add(() => {
+    if (box.intersectsMesh(ground)) {
+      box.material.diffuseColor = new Color3(1, 0, 0);
+    } else {
+      box.material.diffuseColor = new Color3(0.5, 0.5, 0.5);
+    }
+
+    for (let other_box of boxGroup) {
+      if (other_box != box) {
+        if (box.intersectsMesh(other_box)) {
+          box.material.diffuseColor.b = 1;
+        }
+      }
+    }
+  });
+}
 
 /**
  * Render Loop
